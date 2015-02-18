919e2350f6f99199d187237988c09ef8dcf04e80
JDHumphries <jordan.humphries2455@gmail.com>
2015-02-18 18:07:54 -0500

    Added List of Functions
    Created a list of pre-programmed functions for easy use

--- DeltaFountains/gen/com/deltafountains/R.java
+++ DeltaFountains/gen/com/deltafountains/R.java
@@ -29,17 +29,20 @@ public final class R {
         public static final int water_1=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000a;
-        public static final int controlsButton=0x7f080007;
-        public static final int functionsButton=0x7f080008;
+        public static final int action_settings=0x7f08000d;
+        public static final int controlsButton=0x7f080008;
+        public static final int functionsButton=0x7f080009;
         public static final int layout_joystick=0x7f080006;
         public static final int linearLayout1=0x7f080000;
-        public static final int questionImageButton=0x7f080009;
+        public static final int listView=0x7f080007;
+        public static final int questionImageButton=0x7f08000a;
         public static final int textView1=0x7f080001;
         public static final int textView2=0x7f080002;
         public static final int textView3=0x7f080003;
         public static final int textView4=0x7f080004;
         public static final int textView5=0x7f080005;
+        public static final int textView6=0x7f08000b;
+        public static final int textView7=0x7f08000c;
     }
     public static final class layout {
         public static final int activity_controls=0x7f030000;
